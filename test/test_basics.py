87ea758847c7b9555ed4616f8b17df95ad3f3065
sfinlon <finlon@gmail.com>
2020-09-09 11:40:26 -0400

    fix fqdn with trailing slash

--- test/test_basics.py
+++ test/test_basics.py
@@ -4,6 +4,7 @@ from csirtg_re import get
 INDICATORS = {
     '1.1.1.1': 'ipv4',
     'example.com': 'fqdn',
+    'example.com/': 'fqdn',
     '0000:0000::': 'ipv6',
     'a95c530a7af5f492a74499e70578d150': 'md5'
 }
